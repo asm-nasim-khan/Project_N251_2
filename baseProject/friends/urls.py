4d888bed0a9dda8875a818c280e0d3f6f97d8917
A S M Nasim Khan <asmnasimk@gmail.com>
2025-09-08 00:29:02 +0600

    Friend Request sent and approved

--- baseProject/friends/urls.py
+++ baseProject/friends/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('friends/',views.allFriends,name="friends"),
+    path('friends_req/',views.friends_page,name="friends_req"),
+    path("friends/respond/<int:req_id>/", views.respond_friend_request, name="respond_friend_request"),
     path('friend_req/<int:u_id>',views.friend_req,name="send_friend_request"),  
+
       
 ]
